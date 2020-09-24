cdc9317082b1c71e3348e0d8023814e6300b5fab
Maarten Van Giel <maarten.vangiel@icapps.com>
2020-09-24 16:52:30 +0200

    Improve code comments

--- src/main/kotlin/com/gildedrose/item/updater/LegendaryItemUpdater.kt
+++ src/main/kotlin/com/gildedrose/item/updater/LegendaryItemUpdater.kt
@@ -4,12 +4,18 @@ import com.gildedrose.item.Item
 
 class LegendaryItemUpdater : ItemUpdater {
 
+    /**
+     * Legendary items do not update their sellIn value
+     */
     override fun updateSellIn(item: Item) {
-        // No-op: Legendary items do not update their sellIn value
+        // No-op
     }
 
+    /**
+     * Legendary items do not degrade in quality
+     */
     override fun updateQuality(item: Item) {
-        // No-op: Legendary items do not degrade in quality
+        // No-op
     }
 
 }
--- src/main/kotlin/com/gildedrose/item/updater/NormalItemUpdater.kt
+++ src/main/kotlin/com/gildedrose/item/updater/NormalItemUpdater.kt
@@ -8,6 +8,9 @@ open class NormalItemUpdater : ItemUpdater {
         --item.sellIn
     }
 
+    /**
+     * Normal items degrade in quality each day. After reaching the sellIn date, they degrade twice as fast.
+     */
     override fun updateQuality(item: Item) {
         decreaseQuality(item)
         if (item.sellIn < 0) {
@@ -15,12 +18,18 @@ open class NormalItemUpdater : ItemUpdater {
         }
     }
 
+    /**
+     * Decreases the quality of an item by one. The quality of an item cannot drop below a defined minimum (0).
+     */
     protected fun decreaseQuality(item: Item) {
         if (item.quality > Item.QUALITY_MIN) {
             --item.quality
         }
     }
 
+    /**
+     * Increases the quality of an item by one. The quality of an item cannot go above a defined maximum (50).
+     */
     protected fun increaseQuality(item: Item) {
         if (item.quality < Item.QUALITY_MAX) {
             ++item.quality
